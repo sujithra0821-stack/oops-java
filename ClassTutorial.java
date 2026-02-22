a363a26a07c367b07248fd7b820d4c1f846a9bd9
Sujithra <sujithra0821@gmail.com>
2026-02-22 12:19:39 +0530

    init

---
+++ ClassTutorial.java
@@ -0,0 +1,14 @@
+public class ClassTutorial{
+
+    public static void main(String[] args) {
+
+        Cls obj=new Cls(5,10);
+
+        obj.fun();
+        obj.add();
+        
+    }
+
+}
+
+
---
+++ Cls.java
@@ -0,0 +1,21 @@
+public class  Cls{
+
+    public int x;
+    public int y;
+
+    public Cls(int a, int b){
+        this.x=a;
+        this.y=b;
+
+    }
+
+    public void add() {
+        System.out.println(this.x+this.y);
+        
+    }
+
+    public static void fun() {
+        System.out.println("hello world");
+        
+    }
+}
